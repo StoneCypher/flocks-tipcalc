d61c325fbbc831ee28e62567b0b18acf09bd8a6a
John Haugeland <stonecypher@gmail.com>
2015-04-05 18:40:37 -0700

    We now have vertical centering

--- TipCalcLayout.jsx
+++ TipCalcLayout.jsx
@@ -3,12 +3,10 @@ var TipCalcLayout = flocks.createClass({
 
   render: function() {
 
-    var tdstyle = { verticalAlign: 'middle', textAlign: 'center' };
-
     return (
       <table id="layout">
         <tr>
-          <td style={tdstyle}>{this.props.children}</td>
+          <td>{this.props.children}</td>
         </tr>
       </table>
     );

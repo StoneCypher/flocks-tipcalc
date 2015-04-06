86efc890bbeb9643d8992f278568a28c72bee4cd
John Haugeland <stonecypher@gmail.com>
2015-04-05 17:09:27 -0700

    currently unused Tip Calc App

---
+++ TipCalcApp.jsx
@@ -0,0 +1,17 @@
+
+/** @jsx React.DOM */
+/* jshint node: true, browser: true */
+
+'use strict';
+
+
+
+var TipCalcApp = flocks.createClass({
+
+  render: function() {
+
+    return <div>Basic App!</div>;
+
+  }
+
+});

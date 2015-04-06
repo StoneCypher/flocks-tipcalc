f79ea4e7b3dc64314340a5e83a08b20af0c248b5
John Haugeland <stonecypher@gmail.com>
2015-04-05 19:20:35 -0700

    Working bill update; intentionally wrong total update

--- TipCalcPanel.jsx
+++ TipCalcPanel.jsx
@@ -1,14 +1,20 @@
 
 var TipCalcPanel = flocks.createClass({
 
+  update: function() {
+    var NewBill = parseFloat(document.getElementById('billAmount').value);
+    this.fset('bill', NewBill);
+    this.fset('total', NewBill); // TEMPORARY AND WRONG
+  },
+
   render: function() {
 
     return (
 
       <div id="panel">
 
-        <label id="amt" for="billAmount">
-          Bill amount: $<input id="billAmount"/>
+        <label id="amt" htmlFor="billAmount">
+          Bill amount: $<input id="billAmount" onChange={this.update}/>
         </label>
 
         <div id="howHappy">
